7aa6b96ebe9d71ab48c2752baacd896bd0d3b8ff
L_Hadh <hadhemi@elitrack.com>
2020-05-11 14:33:40 +0100

    Display + modification

---
+++ src/components/SimpleObj.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <ul>
+    <template v-for=" (value,key) in anotherObject" >
+      <template v-if="isObject(value)">
+        <li :key="key">{{key}} : {</li>
+          <SimpleObj v-model="anotherObject[key]"/>
+       <li>}</li>
+      </template>
+      <template v-else>
+        <li :key="key">{{key}} : <input @input="valueChanged" type="text" v-model="anotherObject[key]"></li>
+      </template>
+    </template>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'SimpleObj',
+    props: ['value'],
+    data() {
+      return {
+        anotherObject: this.value
+      }
+    },
+    computed: {},
+    methods: {
+      isObject(ob) {
+        return typeof ob == "object"
+      },
+      valueChanged () {
+        this.$emit('input',this.anotherObject)
+      }
+    }
+  }
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import SimpleObj from "./components/SimpleObj.vue";
+
+Vue.component(SimpleObj)
 
 new Vue({
   el: '#app',
